b88ff3b4798e58584f9baaea2aea2ba2c5784708
Mohamedadel4 <36477572+Mohamedadel4@users.noreply.github.com>
2018-03-24 14:38:42 +0200

    Add files via upload

---
+++ code_car.ino
@@ -0,0 +1,56 @@
+char t;
+
+void setup() {
+pinMode(3,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(9,OUTPUT);
+
+Serial.begin(9600);
+
+}
+
+void loop()
+{
+if(Serial.available()){
+t = Serial.read();
+Serial.println(t);
+}
+
+if(t =='1'){
+  digitalWrite(3,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(9,LOW);
+}
+
+else if(t =='2'){
+digitalWrite(3,LOW);
+digitalWrite(5,HIGH);
+digitalWrite(6,LOW);
+digitalWrite(9,HIGH);
+
+}
+else if(t == '3'){
+digitalWrite(3,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,HIGH);
+digitalWrite(9,LOW);
+
+}
+
+else if(t == '4'){
+digitalWrite(3,HIGH);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+digitalWrite(9,LOW);
+}
+
+else if(t == '5'){
+digitalWrite(3,LOW);
+digitalWrite(5,LOW);
+digitalWrite(6,LOW);
+digitalWrite(9,LOW);
+}
+delay(100);
+}
